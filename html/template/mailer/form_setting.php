36ac9b13c96cb29b1162482d108cb99247fe9369
thewarniks <thewarniks@gmail.com>
2025-02-07 18:04:29 +0530

    Second commit

---
+++ html/template/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Nikhil - Portfolio'"; //letter subject
+	$to = 'thewarniks@gmail.com'; // Enter Your E-mail
+	$from = 'thewarniks@gmail.com'; // Admin e-mail
+	$fromName = 'thewarniks'; // Your company name
+	$charset = 'UTF-8';
+?>
